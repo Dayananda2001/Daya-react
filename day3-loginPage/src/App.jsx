ea1f6a6591105c92fda9a0457d3ea2c62c3d0ee0
Dayananda2001 <dayanandajvdaya@gmail.com>
2025-07-03 15:35:47 +0530

    Update App.jsx

--- day3-loginPage/src/App.jsx
+++ day3-loginPage/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import viteLogo from '/vite.svg'
  import { ToastContainer, toast } from 'react-toastify';
-
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [email, setEmail] = useState(" ");
